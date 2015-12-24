15bc7b361aab018d5fcca5914e2adfec15a08396
pedzed <pedzed@outlook.com>
2015-12-24 12:06:07 +0100

    Added SCSS.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+
+gulp.task('scss', function() {
+    gulp
+        .src(['scss/**/*.scss'])
+        .pipe(plumber({
+            errorHandler: function(error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('css/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css/'))
+    ;
+});
+
+gulp.task('default', [
+    'scss',
+]);
+
+gulp.task('watch', function() {
+    gulp.watch('scss/**/*.scss', ['scss']);
+});
